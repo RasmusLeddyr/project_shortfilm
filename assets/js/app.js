46a04eb1cbc351370eb75ecdec600e326e880833
RasmusLeddyr <scud-131@hotmail.com>
2025-04-08 11:29:50 +0200

    Walter ad

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,39 +1,35 @@
-setInterval(function () {
-  //alert("You are an idiot. ;-)");
-  //console.log("*host host*");
-  //console.log("*host*");
-}, 2000);
-
-let isPopupActive = false;
-
 const popupContainer = document.querySelector(".popup-container");
 const popupContent = document.querySelector(".popup-content");
 
+let isPopupActive = false;
+
 function showPopup() {
   if (isPopupActive) return;
-
   const style = window.getComputedStyle(popupContainer);
   const zIndex = parseInt(style.zIndex, 10);
+
   if (zIndex < 0) {
-    isPopupActive = true;
-    alert("I am at your door.");
+    //alert("I am at your door.");
     popupContainer.classList.toggle("popup-active");
     popupContent.classList.toggle("popup-active");
     popupContainer.style.zIndex = "16";
+    setTimeout(function () {
+      isPopupActive = true;
+    }, 4000);
   }
 }
+
 function disablePopup() {
   if (!isPopupActive) return;
-
   const style = window.getComputedStyle(popupContainer);
   const zIndex = parseInt(style.zIndex, 10);
+
   if (zIndex > 0) {
     isPopupActive = false;
     popupContainer.classList.remove("popup-active");
     popupContent.classList.remove("popup-active");
-
     setTimeout(function () {
       popupContainer.style.zIndex = "-4";
-    }, 2000);
+    }, 4000);
   }
 }
